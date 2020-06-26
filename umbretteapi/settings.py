636624a3bab930f8de80aed44d60159eca3aab42
harsha <harshavardhanpillimitla143@gmail.com>
2020-06-26 20:59:00 +0530

    last

--- umbretteapi/settings.py
+++ umbretteapi/settings.py
@@ -149,7 +149,7 @@ REST_USE_JWT = True
 CORS_ORIGIN_ALLOW_ALL =True
 CORS_ORIGIN_WHITELIST = [
   
-    'https://employeeapifrontend.netlify.app/'
+    'https://employeeapifrontend.netlify.app'
 ]
 CORS_ALLOW_HEADERS = [
     'accept',
